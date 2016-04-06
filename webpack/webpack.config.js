9a6e9c323970e2db599fbccc26745934149473fc
Randy Daal <randy@randynamic.com>
2016-04-07 00:18:17 +0200

    fixed `regeneratorRuntime is not defined.` issue

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var parseArgs = require('./parse-args');
 
 module.exports = {
-  entry: ['./src/client/index.js'],
+  entry: ['babel-polyfill', './src/client/index.js'],
   output: {
     path: path.join(__dirname, '..', 'dist', parseArgs.getBuildNumber()),
     filename: 'bundle.js',
